5eb9a0b35002c6405aa08990ec97a3c1b2d95738
Elbadil <elbaliadil@gmail.com>
2023-09-20 23:47:40 +0100

    New State Model

--- models/state.py
+++ models/state.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+
+    cities_states = relationship('City', backref='state',
+                                 cascade='all, delete, delete-orphan')
+
+    @property
+    def cities(self):
+        """returns the list of City instances with state_id equals
+        to the current State.id
+        """
+        return [city for city in models.storage.all(City).values()
+                if city.state_id == self.id]
