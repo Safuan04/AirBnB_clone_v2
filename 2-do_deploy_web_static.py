46125195f1c0a2bddcf920e9b9a1105cd10c3156
Safuan04 <elmail.safuan@gmail.com>
2023-10-06 00:23:56 +0100

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,6 +10,17 @@ import os
 env.hosts = ['54.162.95.185', '54.210.59.144']
 env.user = "ubuntu"
 
+def do_pack():
+    """Generates a .tgz archive from the contents
+    of the web_static folder of this repository.
+    """
+
+    date = datetime.now()
+    now = date.strftime('%Y%m%d%H%M%S')
+
+    local("mkdir -p versions")
+    local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
+
 
 def do_deploy(archive_path):
     """ Deploys """
