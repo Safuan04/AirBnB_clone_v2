3d9c0801734ea4c697e8a2d4d10fbd153b554497
Safuan04 <elmail.safuan@gmail.com>
2023-10-21 17:58:43 +0100

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Impoerting necessary modules"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """This is a rout that displays: Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """This is a rout that displays: HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """Run the application on 0.0.0.0 and port 5000"""
+    app.run(host='0.0.0.0', port=5000, debug=True)
